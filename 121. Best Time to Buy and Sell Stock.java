68bd46d01918c3b20e79b288069243e53a5177a1
Prakash Monis <Prakashmonis06@gmail.com>
2026-08-19 07:06:24 +0530

    first commit

---
+++ 121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int m_price=Integer.MAX_VALUE;
+        int max_profit=0;
+        for(int i=0;i<prices.length;i++){
+
+            if(prices[i]<m_price){
+                m_price=prices[i];
+            }
+            else{
+                int profit=prices[i]-m_price;
+                if(profit>max_profit){
+                    max_profit=profit;
+                }
+            }
+            
+        }
+        return max_profit;
+    }
+}
